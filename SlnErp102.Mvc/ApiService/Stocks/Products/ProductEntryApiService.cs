c1c79c0749f5ae6cb0d020c9b7598ee6cb98bdeb
murattcetinn <muratcetin7@outlook.com>
2022-01-20 20:24:31 +0300

    .

---
+++ SlnErp102.Mvc/ApiService/Stocks/Products/ProductEntryApiService.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using SlnErp102.Api.DTOs.Stocks.Products;
+
+namespace SlnErp102.Mvc.ApiService.Stocks.Products
+{
+    public class ProductEntryApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductEntryApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;  
+        }
+        public async Task<IEnumerable<ProductEntryDistinctDto>?> GetAllAsync()
+        {
+            IEnumerable<ProductEntryDistinctDto>? pEntryDtos;
+            var response= await _httpClient.GetAsync("ProductEntries");
+            //http://localhost:44333/api/productentry
+            if (response.IsSuccessStatusCode)
+            {
+                pEntryDtos = JsonConvert.DeserializeObject<IEnumerable<ProductEntryDistinctDto>>(await response.Content.ReadAsStringAsync());
+
+            }
+            else
+            {
+                pEntryDtos = null;
+               
+            }
+            return pEntryDtos;
+        }
+    }
+}
